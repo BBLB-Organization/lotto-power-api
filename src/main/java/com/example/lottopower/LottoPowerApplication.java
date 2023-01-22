7eb44f9b15e5e2c7fad8ca7b342cbccda2c62759
Jorge Morin <jmorin1@hawk.iit.edu>
2023-01-22 16:13:20 -0600

    Created springboot application to act as backend microservice for front end lotto power

---
+++ src/main/java/com/example/lottopower/LottoPowerApplication.java
@@ -0,0 +1,13 @@
+package com.example.lottopower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LottoPowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LottoPowerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lottopower/LottoPowerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lottopower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottoPowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
