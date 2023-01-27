65353d32fcf389fb1bd8780913a2ac8b87a08ac2
Jorge Morin <jmorin1@hawk.iit.edu>
2023-01-26 21:03:56 -0600

    Front end can now send successful requests to the backend

--- src/main/java/com/example/lottopower/config/SecurityConfig.java
+++ src/main/java/com/example/lottopower/config/SecurityConfig.java
@@ -1,24 +1,50 @@
 package com.example.lottopower.config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
 
 @Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("**")
-                .permitAll()
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .cors()
+                .configurationSource(corsConfigurationSource())
+                .and()
+                .authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .and()
+                .authorizeRequests()
+                .anyRequest().authenticated()
                 .and()
                 .csrf()
                 .disable();
-
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE","PATCH"));
+        configuration.setAllowedHeaders(Arrays.asList("Content-Type"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:4200","https://bblb-organization.github.io/lottery-ai/"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+
 }
 
--- src/main/java/com/example/lottopower/controllers/UserController.java
+++ src/main/java/com/example/lottopower/controllers/UserController.java
@@ -11,7 +11,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@CrossOrigin("*")
 @RequestMapping("user")
 public class UserController {
 
